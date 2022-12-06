5c49d78eeb00d914e0197ba2771c2d29615fe4f6
aryan zarei <aryan2020.zarei@gmail.com>
2022-12-06 19:30:17 +0330

    Update String Method

---
+++ String Method.py
@@ -0,0 +1,25 @@
+print('python3'.capitalize())
+
+
+
+print('PyThoN3'.casefold())
+
+
+
+print('python3'.center(14))
+
+
+
+print('pip install pillow'.count('p'))
+
+
+
+print('a\tb\tc'.expandtabs(4))
+
+
+
+print('python3'.find('p'))
+
+
+
+print('python3'.index('p'))
